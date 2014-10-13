47d5ce371ba69636a0f241c522d8299b8c73e20b
Steffen <Steffen.terheiden@gmx.de>
2014-10-13 18:51:30 +0200

    Sonderausstattungen ein/ausgrauen, Fenstergröße und GuV dynamischer

--- Fallstudie/src/Klomanager/GuV.java
+++ Fallstudie/src/Klomanager/GuV.java
@@ -98,12 +98,23 @@ public class GuV
 			tmp += "<td><b>Verlust</b></td><td colspan='3'>"+(berechneGesamtumsatz() - berechneGesamtkosten())/100.0+"€</td></tr>";
 		}
 		tmp +="<tr><td>Lohnkosten</td><td>"+lohnkosten[0]/100.0+"€</td><td>"+lohnkosten[1]/100.0+"€</td><td>"+lohnkosten[2]/100.0+"€</td></tr>"
-				+"<tr><td>Verwaltungskosten</td><td colspan='3'>"+verwaltungskosten/100.0+"€</td></tr>"
-				+"<tr><td>Zinsaufwendungen<br>Darlehen</td><td colspan='3'>"+zinsaufwendungenDarlehen/100.0+"€</td></tr>"
-				+"<tr><td>Zinsaufwendungen<br>Überziehungskredit</td><td colspan='3'>"+zinsaufwendungenDispo/100.0+"€</td></tr>"
-				+"<tr><td>Aufwendungen für<br>MaFoBericht</td><td colspan='3'>"+(marketingkosten - this.besitzer.getMarketingbudget())/100.0+"€</td></tr>"
-				+"<tr><td>Aufwendungen für<br>Marketing</td><td colspan='3'>"+marketingkosten/100.0+"€</td></tr>"
-				+"<tr><td>Sonderkosten</td><td colspan='3'>"+sonderkosten/100.0+"€</td></tr>";
+				+"<tr><td>Verwaltungskosten</td><td colspan='3'>"+verwaltungskosten/100.0+"€</td></tr>";
+		
+		if(zinsaufwendungenDarlehen !=0){
+			tmp+="<tr><td>Zinsaufwendungen<br>Darlehen</td><td colspan='3'>"+zinsaufwendungenDarlehen/100.0+"€</td></tr>";
+		}
+		if(zinsaufwendungenDispo !=0){
+			tmp+="<tr><td>Zinsaufwendungen<br>Überziehungskredit</td><td colspan='3'>"+zinsaufwendungenDispo/100.0+"€</td></tr>";
+		}		
+		if((marketingkosten - this.besitzer.getMarketingbudget()) !=0){
+			tmp+="<tr><td>Aufwendungen für<br>MaFoBericht</td><td colspan='3'>"+(marketingkosten - this.besitzer.getMarketingbudget())/100.0+"€</td></tr>";
+		}
+		if(this.besitzer.getMarketingbudget() !=0){
+			tmp+="<tr><td>Aufwendungen für<br>Marketing</td><td colspan='3'>"+this.besitzer.getMarketingbudget()/100.0+"€</td></tr>";
+		}	
+		if(sonderkosten != 0){
+			tmp+="<tr><td>Sonderkosten</td><td colspan='3'>"+sonderkosten/100.0+"€</td></tr>";
+		}	
 		if(berechneGesamtumsatz() -berechneGesamtkosten()>=0){
 			tmp+= "<tr><td>Gewinn</td><td colspan='3'>"+(berechneGesamtumsatz() -berechneGesamtkosten())/100.0 +"€</td></tr>";
 		}
